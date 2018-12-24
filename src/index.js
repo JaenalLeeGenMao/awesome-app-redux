fdddfbc9cadf5f8fffb7f6dd0ee7ef1cc6c3ffb9
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2018-12-24 16:30:51 +0700

    test hookswq

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
 import RootReducer from "./reducers";
+
+import Home from "./routes/home";
+
 import "./index.css";
-import App from "./routes/home";
 
 const enhancer = composeWithDevTools(applyMiddleware(thunk));
 const store = createStore(RootReducer, enhancer);
-console.log(store);
+const loggedIn = false;
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter keyLength={12}>
+    <Provider store={store}>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (loggedIn ? <Redirect to="/dashboard" /> : <Home />)}
+        />
+        <Route
+          render={() => {
+            return <div> Page not found </div>;
+          }}
+        />
+      </Switch>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-class Home extends Component {
-  render() {
-    console.log(this.props);
-    return <div> Home yours trully</div>;
-  }
-}
+// class Home extends Component {
+//   render() {
+//     return <div> Home yours trully</div>;
+//   }
+// }
+let multiplication = 1;
+
+const Home = props => {
+  const [count, setCount] = useState(0);
+
+  useEffect(
+    () => {
+      if (count !== 0 && count % 10 === 0) {
+        multiplication += 1;
+      }
+    },
+    [count]
+  );
+
+  return (
+    <div onClick={() => setCount(count + multiplication)}>
+      {" "}
+      Home yours trully, clicked {count} times
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return { ...state };
